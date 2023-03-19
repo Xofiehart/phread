b100a11be5a955f4ea04913dede2f1b57065cb4e
Xofie <S.hart7@rgu.ac.uk>
2023-03-20 00:15:27 +0100

    header footer

---
+++ user/header.php
@@ -0,0 +1,158 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title> Dashboard</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <script src="vendor/jquery/jquery.min.js"></script>
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="">
+
+            <!-- Main Content -->
+            <div id="content">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary" id="accordionSidebar">
+                    <div class="container-fluid">
+
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                                <!-- Sidebar - Brand -->
+                                <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard.php">
+                                    <div class="sidebar-brand-icon rotate-n-15">
+
+                                    </div>
+
+                                </a>
+
+                                <!-- Divider -->
+                                <hr class="sidebar-divider my-0">
+
+                                <!-- Nav Item - Dashboard -->
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="dashboard.php">
+
+                                        <span>Dashboard</span></a>
+                                </li>
+
+                                <!-- Divider -->
+                                <hr class="sidebar-divider">
+
+                                <!-- Heading -->
+
+
+                                <!-- Nav Item - Pages Collapse Menu -->
+                                <li class="nav-item">
+                                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+
+                                        <span>Users</span>
+                                    </a>
+                                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                                        <div class="bg-white py-2 collapse-inner rounded">
+                                            <h6 class="collapse-header">Admin Action:</h6>
+                                            <a class="collapse-item" href="users.php">Delete</a>
+                                            <!-- <a class="collapse-item" href="cards.html">Re</a> -->
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <!-- Nav Item - Utilities Collapse Menu -->
+                                <li class="nav-item">
+                                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+
+                                        <span>Moderate</span>
+                                    </a>
+                                    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+                                        <div class="bg-white py-2 collapse-inner rounded">
+                                            <h6 class="collapse-header">User Input</h6>
+                                            <a class="collapse-item" href="posts.php">Posts</a>
+                                            <a class="collapse-item" href="comments.php">Comments</a>
+                                            <!-- <a class="collapse-item" href="utilities-animation.html">Animations</a>
+                        <a class="collapse-item" href="utilities-other.html">Other</a> -->
+                                        </div>
+                                    </div>
+
+                                </li>
+
+                                <!-- Divider -->
+                                <hr class="sidebar-divider">
+
+                                <!-- Heading -->
+
+                                <!-- Nav Item - Pages Collapse Menu -->
+                                <li class="nav-item">
+                                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+
+                                        <span>Stories</span>
+                                    </a>
+                                    <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                                        <div class="bg-white py-2 collapse-inner rounded">
+                                            <h6 class="collapse-header">Stories:</h6>
+                                            <a class="collapse-item" href="./createPost.php">New Story</a>
+                                            <a class="collapse-item" href="moderatePost.php">Review</a>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <!-- Nav Item - Charts -->
+                                <li class="nav-item">
+                                    <a class="nav-link" href="logout.php">
+
+                                        <span>Logout</span></a>
+                                </li>
+
+                                <!-- Nav Item - Tables -->
+                                <li class="nav-item">
+                                    <a class="nav-link" href="../index.php">
+
+                                        <span>Home</span></a>
+                                </li>
+
+                                <!-- Divider -->
+                                <hr class="sidebar-divider d-none d-md-block">
+
+                                <!-- Sidebar Toggler (Sidebar) -->
+                                <div class="text-center d-none d-md-inline">
+                                    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+                                </div>
+
+
+
+
+                        </div>
+                    </div>
+                </nav>
+                <!-- Topbar -->
+
+                <!-- End of Topbar -->
+
+
+
+            </div>
---
+++ user/register.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>PHRead - Register</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                                <p class="alert">
+
+                                </p>
+                            </div>
+
+                            <form class="user" method="post" action="./register.php">
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="text" name="fname" class="fname form-control form-control-user" id="exampleFirstName" placeholder="First Name">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="text" name="lname" class="lname form-control form-control-user" id="exampleLastName" placeholder="Last Name">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" name="email" class="email form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
+                                </div>
+                                <div class="form-group">
+                                    <input type="mobile" name="mobile" class="mobile form-control form-control-user" id="exampleInputmobile" placeholder="mobile">
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="password" name="password" class="password form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" name="repassword" class="repassword form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <select name="role" class="role form-select form-control form-control-user" aria-label="Default select example">
+                                            <option>select role</option>
+                                            <option value="writer">Writer</option>
+                                            <option value="reader">Reader</option>
+                                        </select>
+
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input class="form-control  btn btn-primary" type="submit" name="Register" value="Register" />
+                                    </div>
+                                </div>
+
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                            </div>
+                            <div class="text-center">
+                                <a class="small" href="login.html">Already have an account? Login!</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+    <script>
+        $(".btn").click(function(e) {
+            e.preventDefault()
+            var fname = $(".fname").val()
+            var lname = $(".lname").val()
+            var email = $(".email").val()
+            var mobile = $(".mobile").val()
+            var password = $(".password").val()
+            var repassword = $(".repassword").val()
+            var role = $(".role").val()
+            var data = {
+                fname,
+                lname,
+                email,
+                repassword,
+                password,
+                mobile,
+                role
+            }
+            $.post("./actions/register.php", data, function(data, status) {
+                console.log('data', data)
+                // var jdata = JSON.parse(data)
+                if (jdata['status'] == 200) {
+                    $(".alert").html(jdata['message'])
+                    setTimeout(() => {
+                        window.location.replace("./login.php");
+                    }, 2000);
+
+
+                } else {
+                    $(".alert").html(jdata['message'])
+                }
+            });
+        });
+    </script>
+</body>
+
+</html>
